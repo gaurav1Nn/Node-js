576d9c571a87d93f90f2cd3c38aae431afc8f82a
Delta Student gaurav <119969595+gaurav1n@users.noreply.github.com>
2024-09-29 12:28:22 +0530

    add new file

---
+++ todo/todo.js
@@ -0,0 +1,42 @@
+const { log } = require('console')
+const fs = require('fs')
+const filepath = "./tasks.json"
+
+const loadTasks = () =>{
+    try {
+        const dataBuffer = fs.readFileSync(filepath)
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    }catch(error){
+        return []
+    }
+}
+const listTasks = () => {
+    const tasks = loadTasks()
+    tasks.forEach((task , index) => console.log(`${index +1} - ${task.task}`));
+};
+const saveTasks = (tasks) => {
+   const dataJSON =  JSON.stringify(tasks)
+   fs.writeFileSync(filepath , dataJSON);
+};
+const addTask = (task) => {
+    const tasks = loadTasks()
+    tasks.push({task});
+    saveTasks(tasks); 
+    console.log("task added", task);
+    
+};
+const command = process.argv[2];
+const argument= process.argv[3];
+if(command === 'add'){
+    addTask(argument)
+} else if(command === "list"){
+    listTasks();
+}
+else if( command === "remove"){
+    removeTask(parseInt(argument));
+}
+else{
+    console.log("command not found!");
+    
+}
